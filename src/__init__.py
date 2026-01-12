22fdfa8000c530826516f859c060f0eef7449458
Fyre-Aspect <aamirtinwala7@gmail.com>
2026-01-11 19:26:15 -0500

    Init

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+# Job Search Automation Tool
+# Source package initialization
---
+++ src/scrapers/__init__.py
@@ -0,0 +1,7 @@
+# Scrapers package
+from .indeed_rss import IndeedRSSScraper
+from .remoteok import RemoteOKScraper
+from .themuse import TheMuseScraper
+from .adzuna import AdzunaScraper
+
+__all__ = ['IndeedRSSScraper', 'RemoteOKScraper', 'TheMuseScraper', 'AdzunaScraper']
